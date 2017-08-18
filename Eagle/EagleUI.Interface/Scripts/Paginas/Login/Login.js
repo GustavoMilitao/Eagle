63c3ebff7eab7916e53cceeb81a4c3b6ff5f7c7d
Militão <gustavo.h.militao@gmail.com>
2017-08-18 18:45:30 -0300

    Ajax to login

---
+++ Eagle/EagleUI.Interface/Scripts/Paginas/Login/Login.js
@@ -0,0 +1,14 @@
+﻿$('#login-form').submit(function () {
+    $.ajax({
+        type: 'POST',
+        url: '/Login',
+        data: {
+            user: $('#user').val(),
+            senha: $('#password').val(),
+            keepLogged: $('#keep-logged').prop('checked') 
+        }, 
+        success: function (data){
+
+        }
+    });
+});
